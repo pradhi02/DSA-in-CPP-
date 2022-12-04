1647055e1d0e39f5454034b87d797b7d912ce813
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-04 11:15:15 +0530

    Add files via upload

---
+++ que10.cpp
@@ -0,0 +1,48 @@
+/*#include<iostream>
+using namespace std; 
+int main(){ 
+    int age , section; 
+    cout<<"enter the age and section of the student"<<endl; 
+    cin>> age, section;  
+     if ( age >=18 && section>=18)
+     {
+        cout<<"students are allowed for the helloween party"<<endl; 
+        
+     }  
+   else{cout<<"not allowed for the party ";} 
+
+
+    
+
+    return 0; 
+}  */ 
+
+
+
+#include<iostream>
+using namespace std; 
+int main(){ 
+    int n; 
+    cout<<"enter the integer n"<<endl; 
+    cin>>n; 
+    if (n>5)
+    {
+        cout<<"the number entered is greater than 5"; }
+         
+         else if (n<5){
+            cout<<"less than 5"<<endl; 
+        } 
+        else{
+            cout<<"entered number is equal to 5"<<endl;
+        }
+    
+    
+
+
+
+
+
+
+
+    return 0 ; 
+}
