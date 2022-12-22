7fb77c16aeeda85d022da8180acda97a38f4fba3
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-22 18:30:20 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2; 
+    cout<<"enter the value of num1:\n";// << this symbol is insertion operato in c++ ; output screen pr dedo; 
+    cin>>num1; // >> this is extraction operator; it means extract the input ; input lelo 
+    
+    cout<<"enter the value of num2:\n";
+    cin>>num2; // >> this is extraction operator; it means extract the input ; input lelo  
+    cout<< "the sum is "<< num1+num2;
+     
+    
+
+    
+
+     
+    return 0;
+}
+
+
