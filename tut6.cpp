691d3bbc611caf74eb2a0bd2b6a6c8fc54a56108
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-24 11:40:28 +0530

    Add files via upload

---
+++ tut6.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std; 
+// selection structure if else statements  if else=if else ladder //
+int main(){ 
+ int age; 
+ cout<<"enter your age"<<endl;
+ cin>>age;
+//if (age==10)
+ //{
+   // cout<<"you are 10 year old, you can go to big  park rides";}
+   // else if(age<10) {cout<<"you cannot go to the big ride";}
+
+//else if (age==12)
+//{ cout<<"you will get kids pass"; 
+//}
+
+
+        //else{ cout<<"you are always welcomee";}
+   
+   
+    
+//}
+
+
+
+//selection structute in switch case statements  
+  switch (age)
+  {
+  case 12:
+    cout<<"age is 12 man"; 
+    break;    //agar break hatadenege to sare cases print ho jatein hai 
+  
+  default: 
+  cout<<"nothing to show";
+    break;
+  }
+} 
