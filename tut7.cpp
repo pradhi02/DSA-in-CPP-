cef649407483ba093471cc50050698820f5f9e67
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-26 11:13:07 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std; 
+int pradhi=12;
+int main(){ 
+
+ int pradhi=13;  
+ cout<<::pradhi;
+
+
+
+ /**loops in c++ ;/ 
+ there  are three types of loops in c++ namely- 
+ 1. for loop 
+ 2. while loop 
+ 3. do while loop 
+ 
+
+ /*for loop in c++*/  
+ //syntax for : for loop- 
+
+ // for(initilization; condition; updation;)
+  // {
+   // loop body (c++ code);
+  //  }  
+
+ //for ( int i=0 ; i < 5; i++)
+ //{
+    /* code */ 
+   // cout<< i<<endl ; //agar 2 baar i++ likh doge to 2 baar updation ho jaygiegi islye 2 baar i++ ni likhna//
+ //}
+ 
+ //while loop in c++ 
+//syntax of while loop//
+ //while (/* condition */)
+ //{
+    /* code */
+   // statements in c++  //keeps on executing jab tak condition true rehte hain! 
+ //}  
+ //int i=0; 
+ //while (true) {
+    /* code */ 
+    //cout<<i<<endl;
+    //i++;
+ // }
+  //agr hum infinite loop likhte hain to vo jyda memory drain krega which can drain our memory system  
+  //int i=1;
+ // do
+ // {
+   ////  cout<<i<<endl;
+     //++;  
+ // } while (i<40);
+  
+
+return 0 ;   
+
+
+
+}
