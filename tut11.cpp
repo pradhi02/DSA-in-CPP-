6544d67074f14a06254515e0a928e04a366d26e0
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-29 17:10:01 +0530

    Add files via upload

---
+++ tut11.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;      
+
+
+
+ // typedef struct  pradhi //typedef k mtlb hai jisko phele struct pradhi bol rhai the usko ab ep bolenge// 
+ // {
+
+ //int age;  
+ //char favcolour;
+ //float salary;
+ //double income; 
+
+ // }ep ;  
+     
+  //   int main() { {
+ //ep Day1;
+ //ep Day2;
+ 
+ 
+ //Day1.age=18;
+ //Day1.favcolour='black';
+ //Day1.salary=35456;
+ //Day1.income=7868998;
+  //cout<<"the value of pradhi salary is "<<Day1.salary<<endl;  
+  //cout<<"the value of pradhi colour is"<<Day1.favcolour<<endl;  
+  //cout<<"the value of pradhi income is "<<Day1.income<<endl;  
+ // cout<<"the value of pradhi salary is "<<Day1.age<<endl<<endl<<endl<<endl;  
+   
+  // Day2.age=19;
+  // Day2.income=83628736;
+  // Day2.salary=33893;
+  // Day2.favcolour='red';
+  //// cout<<"the value of pradhi day2 is"<<Day2.salary<<endl;
+  // cout<<"the value of pradhi day2 is"<<Day2.favcolour<<endl;
+  // cout<<"the value of pradhi day2 is"<<Day2.income<<endl;
+  // cout<<"the value of pradhi day2 is"<<Day2.age<<endl;
+  // }; 
+    
+//typedef union money
+//{
+ // int rice;
+ // char car;
+ // float pounds;
+//} ppp;  
+ 
+
+ 
+
+
+
+int main(){ 
+   
+   enum meal {pradhi, jhhjghjv, happy}; 
+   meal m1=pradhi;
+   cout<<m1;
+   
+   
+
+   //ppp m1;
+   //union money m2;
+   //m1.rice=54; 
+   //m1.car='p'; //ab rice overwirte ho jayega or rice ki correct value ni garbage value milegi//
+   //cout<<m1.rice;//yha garbage value print  hogi kyuki, ab value overwite hogye hai! yha m1.care ki value sahi milegi. //union me hum ek baar me ek hi9 value use kr sakte hai, example money barter system, kyuki apki memory jo hai vo share ki jari hai/.
+   
+
+
+
+   
+ 
+
+  
+
+
+
+    return 0; }
