f1fafbb34344a3008ce455ee3d420caab7cdd2e4
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-30 10:43:08 +0530

    Add files via upload

---
+++ tut20.cpp
@@ -0,0 +1,54 @@
+/*#include<iostream>
+#include<string>
+using namespace std;  
+int main(){ 
+
+string str; 
+//getline(cin,str);
+cout<<"enter the string"<<endl;
+cin>>str;
+cout<<"the entered string is"<<str<<endl;    
+cout<<"using getline function"<<endl;
+getline(cin,str);
+cout<<"enter string"<<str<<endl;
+cout<<"enter the character"<<endl;
+cout<<"using pushback function"<<str<<endl;
+str.push_back('s'); 
+cout<<"after using push back the string"<<str<<endl; 
+str.pop_back();
+cout<<"after deleting the string"<<str;
+
+ return 0; 
+}
+*/ 
+
+
+
+ /*#include<iostream>
+#include<string>
+using namespace std; 
+int main(){
+    string str="HAPPPY BIRTHDAY BEAN";
+    cout<<"the initial string is"<<str<<endl; 
+    cout<<"the length of the string is"<<str.length()<<endl; 
+
+    cout<<"the size of string after the change is"<<endl;
+    str.resize(15); 
+    cout<<"the new string is"<<str<<endl; 
+     str.capacity();
+     cout<<str.capacity()<<endl;
+
+     cout<<"lets us try to fit in the string "<<endl; 
+     str.shrink_to_fit();
+     cout<<"the string after shrink is "<<str<<endl; 
+    return 0 ; 
+} */
+
+
+#include<iostream>
+using namespace std; 
+int main(){
+
+
+    return 0 ; 
+}    
