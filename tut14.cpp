e49a265c2f5069f99225a2da6278a27a211f248d
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2022-12-31 10:31:47 +0530

    Add files via upload

---
+++ tut14.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+ inline int hola(int a, const float b=2.5){
+
+    return a*b;
+}
+int main(){ 
+    int a= 12; int b =233; 
+   cout<<"the value of hi is  " <<hola(12,233)  <<"also hols dgfuhgeyf"<<endl; 
+cout<<"hii";
+
+    return 0; h 
+}
