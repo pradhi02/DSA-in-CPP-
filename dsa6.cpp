ec0e90dbf6c2de45df352410e8f0da91089c64ec
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-01-20 09:54:56 +0530

    Add files via upload

---
+++ dsa6.cpp
@@ -0,0 +1,29 @@
+/*#include<iostream> 
+using namespace std;
+int main(){
+int a; 
+cin>>a;
+for (int i = 1; i <10; i++)
+{
+   cout<<i*a<<endl; 
+   if (i==5)
+   {
+    continue;
+   }
+   
+}
+
+}*/ 
+
+/*#include<iostream> 
+using namespace std; 
+int main(){ 
+    for (int i = 0; i <=5; i--)
+    {
+        cout<<i<<endl;
+        i++; 
+        
+    } return 0;
+}*/ 
+ 
+ 
