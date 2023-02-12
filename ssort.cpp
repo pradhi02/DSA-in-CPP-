ba2a8acca7bff6dea73c95889c1c0092c0f3ac67
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-12 10:10:24 +0530

    Add files via upload

---
+++ ssort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std; 
+void selectionsort(int array[],int size){ 
+  
+    for (int i = 0; i < size; i++)
+    {   int minindex=i;
+        for (int j = i+1; j< size; j++)
+        {
+            if (array[j]<array[minindex])
+            {
+            minindex=j;
+            } 
+           
+            
+        } 
+         swap(array[minindex],array[i]);
+    }
+    
+}
+int main(){ 
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }  
+    selectionsort(arr,n);
+
+    for (int i = 0; i < n; i++)
+    {
+     cout<<arr[i]<<endl;
+    }
+    
+    
+    
+
+}
