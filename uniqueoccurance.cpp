ab1ac26e15c52074a55db1a9714db6352f904f25
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-13 10:07:47 +0530

    Add files via upload

--- uniqueoccurance.cpp
+++ uniqueoccurance.cpp
@@ -1,36 +1,24 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
-int  uniqueoccurance(int array[],int size){ 
-     
-    int count=1; int ans=0;
-    for (int i = 0; i < size; i++)
-    {
-        if (array[i]==array[i+1]) //checking occurance 
-        {
-            count++;//count will store the number of time element occured
-        } 
+int arraySum(int array[],int size) {
+    int sum=0;
+    for (int i=0; i<size; i++) { 
+        sum=sum+array[i];
+     } 
+     return sum;
+}
 
-        else count=1;
- }  
- 
-ans=(ans^count);  
- return ans;}
-int main(){
+int main(){ 
     int n;
     cin>>n;
-    int arr[n];  
-    bool unique=uniqueoccurance(arr,n);
-    for (int i = 0; i < n; i++)
-    {
+    int arr[n]; 
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     } 
-    if (unique)
-    {
-       cout<<"true";
-    }
-    else{
-        cout<<"false";
-    }
+    int index=arraySum(arr,n);
+    cout<<index;
 
-return 0;
+    
+    
+    return 0;
 }
