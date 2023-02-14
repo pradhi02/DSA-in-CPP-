6e4253515d923616ff3c06ea97f50e8b91680a13
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-14 09:36:43 +0530

    Add files via upload

---
+++ bbsort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std; 
+int bubbleSort(int arr[],int size) {
+ int counter=1;
+ while (size-counter>=1)
+ {
+ for (int i = 0; i <size; i++)
+ {
+    if (arr[i]>arr[i+1])
+    {
+        swap(arr[i],arr[i+1]);
+    }
+    else 
+ }
+ 
+ }
+ 
+
+
+}
+int main(){ 
+
+    return 0;
+}
