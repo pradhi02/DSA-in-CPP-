89b13568339e72efe4fbe1e83f56f04696424185
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-16 09:57:02 +0530

    Add files via upload

--- hacrank.cpp
+++ hacrank.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std; 
 void plusMinus(int array[],int size){
     long double P=0,N=0,Z=0;
@@ -35,4 +35,41 @@ int main(){
     long double index =plusMinus(arr,n); 
     
     return 0; 
+}*/ 
+
+#include<iostream>
+using namespace std; 
+int compareTriplets(int A[3],int B[3]){ 
+    int apoint=0; int bpoint=0;
+    for (int i=0; i<3; i++) { 
+        if (A[i]>B[i]) {
+        apoint++;
+        }
+        else if (A[i]<B[i]) {
+            
+         bpoint++;
+        } 
+        else{
+            continue;
+        }
+    } 
+    return apoint,bpoint;
+}
+int main(){ 
+    
+    int Aar[3];
+    for(int i=0;i<3;i++){
+        cin>>Aar[i];
+        
+    }  
+    int Bar[3];
+    for(int i=0;i<3;i++){
+        cin>>Bar[i];
+    } 
+    int index=compareTriplets(Aar,Bar);
+    cout<<index;
+
+    
+    
+    return 0;
 }
