bb3a5ab922182bca28468c89974dfebe079ad8e4
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-20 10:20:05 +0530

    Add files via upload

--- hacrank.cpp
+++ hacrank.cpp
@@ -37,7 +37,7 @@ int main(){
     return 0; 
 }*/ 
 
-#include<iostream>
+/*#include<iostream>
 using namespace std; 
 int compareTriplets(int A[3],int B[3]){ 
     int apoint=0; int bpoint=0;
@@ -73,3 +73,112 @@ int main(){
     
     return 0;
 }
+*/ 
+
+ 
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n; 
+    for (int i = 1; i<=n; i++)
+    {
+        for (int j=0; j<n; j++){ 
+            if (j<n-i)
+            {
+                cout<<" ";
+            }
+            else { 
+                cout<<"*";
+            }
+
+        } 
+        cout<<endl;
+        
+    }
+    
+    
+    
+    
+    
+    return 0;
+} */ 
+
+
+/*#include<iostream>
+using namespace std; 
+int catAndMouse(int x,int y,int z){ 
+    int a,b;
+    if (x>z)
+    {
+       a=x-z;
+    } 
+    else{
+        a=z-x;   
+    }
+     if (y>z)
+     {
+        b=y-z;
+     } else{
+        b=z-y;
+     }
+     if (a>b)
+     {
+        cout<<"Cat A";
+     } 
+     else if (b>a)
+     {
+        cout<<"Cat B";
+     } 
+     else{
+        cout<<"Mouse C";
+     }
+         
+
+     
+     
+
+}
+int main(){ 
+    
+    return 0;
+}*/ 
+
+#include<iostream>
+using namespace std; 
+int plusMinus(int array[],int size){ 
+    long double  P=0,N=0,Z=0;
+    for (int i = 0; i < size; i++)
+    {
+ 
+ if (array[i]>0)
+ {
+    P++;
+} else if (array[i]<0)
+{
+    N++;
+} 
+else{
+    Z++;
+} 
+} 
+
+cout<<P/size<<endl;
+cout<<N/size<<endl;
+cout<<Z/size<<endl;
+    
+}
+int main(){ 
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i <n; i++)
+    {
+        cin>>arr[i];
+    } 
+    long A=plusMinus(arr,n);
+    cout<<A;
+    
+
+    return 0;
+    }
