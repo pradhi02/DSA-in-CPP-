419055c966020b39d5d9fd64bc769a59c0c3fb86
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-02-21 11:26:38 +0530

    Add files via upload

---
+++ birthdaycakecandle.cpp
@@ -0,0 +1,40 @@
+/*#include<iostream>
+#include<climits>
+using namespace std;
+int birthdayCakeCandle(int array[],int size){
+    int maxno=INT_MIN;  int count=0;
+    for (int i = 0; i < size; i++)
+    {
+    maxno=max(maxno,array[i]);
+    } 
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (maxno=array[i])
+        {
+            count++;
+        }
+        
+    } 
+    return count;
+    
+    
+}
+int main(){   
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+    cin>>arr[i];
+    } 
+    
+    int index=birthdayCakeCandle(arr,n);
+ return 0;
+}*/
+#include<iostream>
+using namespace std;
+int main(){ 
+    
+    return 0;
+}
