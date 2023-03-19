668818962a6e9728e4d55f399b8547069b584b60
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-03-19 11:33:09 +0530

    Add files via upload

--- p2.cpp
+++ p2.cpp
@@ -1,10 +1,38 @@
+/*#include<iostream>
+using namespace std; 
+int countOdds(int low, int high) {  
+    int count;
+    int i;
+        while (low<=i<=high)
+        { count=0;
+      if ((i/2)==0)
+      {
+        count++;
+      }
+      
+        }
+        
+      return count;
+        
+    }
+int main(){  
+    int a=3,b=7;
+    cout<<countOdds(3,7);
+
+    return 0;
+}*/
 #include<iostream>
 #include<vector>
-void rotate(vector<int>v,int k){
-
-}
-using namespace std;
-int main(){ 
+using namespace std; 
+double avgsalary(vector<int>salary){
+    int max=salary[0],min=salary[0];
+    for (int i = 0; i < salary.size(); i++)
+    {
+        if()
+    }
     
+}
+int main(){
     return 0;
+
 }
