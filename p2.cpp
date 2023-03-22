71400413877568e8f1f718b1edebd8a5dfe9d9d5
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-03-22 11:53:14 +0530

    Add files via upload

--- p2.cpp
+++ p2.cpp
@@ -21,18 +21,47 @@ int main(){
 
     return 0;
 }*/
+
+/*#include<iostream>
+#include<vector>
+using namespace std;
+int signfunc(vector<int>v){
+  int ans=1;
+  for(int i=0;i<v.size();i++){
+    ans=ans*v[i];
+  } 
+  if(ans>0) return 1;
+  else if(ans<0) return -1;
+  else return 0;
+}
+int main(){  
+  vector<int>v;
+  v.push_back(2);
+  v.push_back(-2);
+  v.push_back(-2);
+  v.push_back(-2);
+  v.push_back(2); 
+  int temp=signfunc(v);
+  cout<<temp;
+
+
+  return 0;
+}*/
+
 #include<iostream>
 #include<vector>
-using namespace std; 
-double avgsalary(vector<int>salary){
-    int max=salary[0],min=salary[0];
-    for (int i = 0; i < salary.size(); i++)
-    {
-        if()
-    }
-    
+#include<algorithm>
+using namespace std;
+bool ap(vector<int>v){ 
+  sort(v.begin(),v.end());
+  for (int i = 0; i < v.size(); i++)
+  {
+    int cd=(v[i+1]-v[i]);
+    for
+  }
+  
 }
-int main(){
-    return 0;
+int main(){ 
 
+  return 0;
 }
