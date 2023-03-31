0eb8a952b22fc33917a2e635d675fccce4032f9a
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-03-31 12:25:40 +0530

    Add files via upload

--- math.cpp
+++ math.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std; 
 bool isPrime(int n){
   if(n<=1)
@@ -18,5 +18,37 @@ int main(){
     cin>>num;
     cout<<isPrime(num);
     
+    return 0;
+}*/  
+
+#include<iostream>
+using namespace std;  
+//recursion method 
+/*int gcd(int a,int b){
+    if(a==0)
+    return b; 
+    gcd(b%a, a); 
+}*/ 
+
+//iteration method 
+int gcd(int a,int b){
+    if(a==0){
+        return b;
+    } 
+    if(b==0){
+        return a; 
+    } 
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        } 
+        else { b=b-a;}
+    } 
+    return a;
+}
+int main(){
+    //int n1,n2;
+    //cin>>n1>>n2;
+    cout<<gcd(5,10);
     return 0;
 }
