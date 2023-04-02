cb6094697a76686d021e0c188c9a1310b5797174
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-04-02 10:21:09 +0530

    Add files via upload

---
+++ segmentedsieve.cpp
@@ -0,0 +1,53 @@
+/*#include<iostream> 
+#include<vector> 
+/*int primeNum(int n){ 
+    int count=0;
+    vector<bool>prime(n+1,true); 
+    prime[0]=prime[1]=false;
+    for(int i=2;i<n;i++){
+        if(prime[i]){
+            count++;
+        
+        for(int j=2*i;j<n;j=j+i){
+        prime[j]=false; 
+        }  
+        } 
+    } 
+    return count;
+
+} */ 
+/*
+int countPrimes(int n) { 
+        int count=0;
+        vector<bool>prime(n+1,true); 
+        prime[0]=prime[1]=false;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+          count++;  
+  //j=i+i isloye kyuki i k table me ane walem her number ko nonprime mark krna hai ab 
+          for(int j=2*i;j<n;j=j+i){ 
+              prime[j]=false;
+          } 
+        }
+        }
+     return count;   
+    }
+
+using namespace std;
+int main(){ 
+    
+    
+    return 0;
+}*/  
+ 
+/* #include<iostream>
+ using namespace std; 
+ int countPrime(int num1,num2){
+    int count=0;
+    vector<bool>prime(h-1+1,true) 
+ }
+ int main(){ 
+    cout<<<<endl;
+    return 0;
+ }*/ 
+ 
