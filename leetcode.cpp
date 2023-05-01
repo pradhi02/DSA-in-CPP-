dedd6d812efab9448fb0938fe8cedf241e739abe
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-05-01 10:38:51 +0530

    Add files via upload

---
+++ leetcode.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+int mountainArray(int array,int size){
+    int count=0;
+    
+    for (int i = 0; i <size ; i++)
+    {
+        for (int j = 1; j < size; j++)
+        {
+            if (array[i]==array[j])
+            {
+                count++;
+            } 
+              
+        }
+        return co
+        
+    }
+    
+   
+    
+}
