902514bf059ff67742ca377cc68d9e7df1fb4b0b
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2023-05-07 09:34:11 +0530

    Add files via upload

---
+++ R1.cpp
@@ -0,0 +1,64 @@
+/*#include<iostream>
+using namespace std;  
+void printnumber(int num){ 
+    //base case  
+    if( num<=0){ 
+        return; 
+    }   
+    
+  recursive call   
+printnumber(num-1); 
+cout<<num<<endl; 
+
+return;
+    
+}
+int main(){   
+ printnumber(5);
+
+     
+    return 0;
+}*/ 
+
+/*#include<iostream>
+using namespace std;  
+void printnum(int n){
+  //basecase
+  if(n==6){
+    return;
+  } 
+  cout<<n;
+  printnum(n+1);
+}
+int main(){ 
+  cout<<"hello world"; 
+  printnum(1);
+  return 0;
+}*/  
+/*
+#include<iostream>
+using namespace std;  
+int sumnum(int i, int n, int sum=0){
+ 
+  //base case 
+  if(i==n){  
+    sum=sum+i;
+    return sum; 
+  }  
+  sum=sum+i;
+  //recursive case 
+  sumnum(i+1,n,sum);
+
+}
+int main(){  
+  
+  cout<<sumnum(1,5,0);
+
+  return 0;
+}*/ 
+
+#include<iostream>
+using namespace std; 
+int main(){
+  return 0;
+}
